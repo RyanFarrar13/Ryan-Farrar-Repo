e9d5ab528155a015eb56968cd280046706bd702e
RyanFarrar13 <116859109+RyanFarrar13@users.noreply.github.com>
2023-05-18 15:21:18 -0400

    Add files via upload
    Just a 'hello world' intro file, will upload rest of catalogue over next few months.

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+public class HelloWorld{
+	public String WOAH;
+	public static void main (String args[])){
+		String WOAH = 'Hello World';
+		System.out.println(WOAH);
+		
+		
+		
+		
+	}
+
+}//end of class
